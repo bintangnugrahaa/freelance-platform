c50cbdd4d3df667e3f3f233572c3eb760625f2f3
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2025-05-19 00:36:04 +0700

    implement top-up wallet transaction feature

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,9 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreTopupWalletRequest;
+use App\Models\WalletTransaction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
-    //
+    public function wallet()
+    {
+        $user = Auth::user();
+
+        // topup, withdrawal, revenue, expense
+        $wallet_transactions = WalletTransaction::where('user_id', $user->id)
+            ->orderByDesc('id')
+            ->paginate(10);
+
+        return view('dashboard.wallet', compact('wallet_transactions'));
+    }
+
+    public function withdraw_wallet()
+    {
+        return view('dashboard.withdraw_wallet');
+    }
+
+    public function topup_wallet()
+    {
+        return view('dashboard.topup_wallet');
+    }
+
+    public function topup_wallet_store(StoreTopupWalletRequest $request)
+    {
+        $user = Auth::user();
+
+        DB::transaction(function () use ($request, $user) {
+            $validated = $request->validated();
+
+            if ($request->hasFile('proof')) {
+                $proofPath = $request->file('proof')->store('proofs', 'public');
+                $validated['proof'] = $proofPath;
+            }
+
+            $validated['type'] = 'Topup';
+            $validated['is_paid'] = false;
+            $validated['user_id'] = $user->id;
+
+            WalletTransaction::create($validated);
+        });
+
+        return redirect()->route('dashboard.wallet');
+    }
 }
